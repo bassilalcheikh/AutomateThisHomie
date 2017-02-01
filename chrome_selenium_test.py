7d852b58b963f029b22470768cb69e1486cbfd7d
bassilalcheikh <bassil.alcheikh@gmail.com>
2017-02-01 16:40:26 -0600

    Minor updates; wrapped code into some functions

--- chrome_selenium_test.py
+++ chrome_selenium_test.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 import time
 
-driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
+url = 'www.example.com'
+driver = webdriver.Chrome()
 
 def login_entry(username, password, browser):
     login_email = browser.find_element_by_id('UserName')
@@ -10,16 +11,18 @@ def login_entry(username, password, browser):
     login_password.send_keys(password)
     submit_elem = browser.find_element_by_xpath("//button[contains(text(), 'Log in')]")
     submit_elem.click()
+    
+def record_source_code(file_name, browser):
+    time.sleep(5)
+    file_destination = open(file_name, 'w')
+    file_destination.write(repr(browser.page_source))
+    file_destination.close()
 
-driver.get('www.example.com')
+driver.get(url)
 time.sleep(3) # Let the user actually see something!
 login_entry('username', 'password', driver)
-#search_box.send_keys('ChromeDriver')
-#search_box.submit()
-time.sleep(5)
-test_output = open('chrome_selenium_test_source_output.html', 'w')
-test_output.write(repr(driver.page_source))
-test_output.close()
+
+record_source_code('chrome_selenium_test_source_output.html', driver)
 
 time.sleep(5) # Let the user actually see something!
 driver.quit()
